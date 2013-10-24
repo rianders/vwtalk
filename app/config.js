5846c342052636ac3548ca5e52afac7e845fe4e6
Ricklon <rick.rickanderson@gmail.com>
2013-10-24 18:04:09 -0400

    end of day update. need remove duplicate logins. need to delcare error when no device available.

--- app/config.js
+++ app/config.js
@@ -5,5 +5,5 @@ config.opentok.key = "10120602";
 config.opentok.secret = "41c132f9c35f3278f4d2b78b8f87bb4b94210b3d" ;
 config.opentok.location = "localhost";
 config.port = (process.env.PORT || 8080);
-config.host = (process.env.HOST || 'localhost');
+config.host = (process.env.HOST || "165.230.192.24");
 module.exports = config;
