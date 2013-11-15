525a72d6a7a7f723c51291faa805d7f35b0e6e48
Rick Anderson <rianders@docs.rutgers.edu>
2013-11-15 16:04:13 +0000

    Iniitial cleanup after test demo.

--- app/public/js/opentok.js
+++ app/public/js/opentok.js
@@ -1,81 +1,66 @@
-    var currentRoom;
-    var room = "room1";
-    var user;
-    var sessions = {};
-    var globaldata = {};
-    var publisher;
-    var isMicOn = true;
-    var sessionToJoinOnStart;
-    var subscribed = false;
-	
-    TB.setLogLevel(TB.DEBUG);
-  
+var currentRoom;
+var room = 'room1';
+var user;
+var sessions = {};
+var globaldata = {};
+var publisher;
+var isMicOn = true;
+var sessionToJoinOnStart;
+var subscribed = false;
+TB.setLogLevel(TB.DEBUG);
+
 function subscribeToStreams(streams) {
-    console.log("subscribeToStreams");
-
-    for (var ii = 0; ii < streams.length; ii++) {
-        console.log("stream ii: " + streams.length + ", StreamName: " + streams[ii].name);
-        // Make sure we don't subscribe to ourself
-        if (streams[ii].connection.connectionId == sessions[currentRoom].connection.connectionId) {
-            return;
-        }
-
-        // Create the div to put the subscriber element in to
-        var div = document.createElement('div');
-        div.setAttribute('id', 'stream' + streams[ii].streamId);
-        var streamsContainer = document.getElementById('streamsContainer');
-        streamsContainer.appendChild(div);
-
-        var subProperties = new Object();
-        subProperties.height = 100;
-        subProperties.width = 128;
-        subProperties.style = {};
-        subProperties.style.nameDisplayMode = "on";
-
-        var subscriber = sessions[currentRoom].subscribe(streams[ii], 'stream' + streams[ii].streamId, subProperties);
-       // subscriber.subscribeToVideo(false).subscribeToAudio(true);
-
+  console.log('subscribeToStreams');
+  for (var ii = 0; ii < streams.length; ii++) {
+    console.log('stream ii: ' + streams.length + ', StreamName: ' + streams[ii].name);
+    // Make sure we don't subscribe to ourself
+    if (streams[ii].connection.connectionId == sessions[currentRoom].connection.connectionId) {
+      return;
     }
-
+    // Create the div to put the subscriber element in to
+    var div = document.createElement('div');
+    div.setAttribute('id', 'stream' + streams[ii].streamId);
+    var streamsContainer = document.getElementById('streamsContainer');
+    streamsContainer.appendChild(div);
+    var subProperties = {};
+    subProperties.height = 100;
+    subProperties.width = 128;
+    subProperties.style = {};
+    subProperties.style.nameDisplayMode = 'on';
+    var subscriber = sessions[currentRoom].subscribe(streams[ii], 'stream' + streams[ii].streamId, subProperties);  // subscriber.subscribeToVideo(false).subscribeToAudio(true);
+  }
 }
 
-    function streamCreatedHandler(event) {
-        console.log("streamCreateHandler");
-        subscribeToStreams(event.streams);
-    }
-    function sessionConnectedHandler(event) {
-        console.log("sessionConnectedHandler");//: cnt:  " +  event.connections.length);
+function streamCreatedHandler(event) {
+  console.log('streamCreateHandler');
+  subscribeToStreams(event.streams);
+}
 
-        // Create publisher and start streaming into the session
-        var div = document.createElement('div');
-        div.setAttribute('id', 'publisher');
+function sessionConnectedHandler(event) {
+  console.log('sessionConnectedHandler');
+  //: cnt:  " +  event.connections.length);
+  // Create publisher and start streaming into the session
+  var div = document.createElement('div');
+  div.setAttribute('id', 'publisher');
+  var publisherContainer = document.getElementById('publisherContainer');
+  // This example assumes that a publisherContainer div exists
+  publisherContainer.appendChild(div);
+  //set publishing options
+  var pubOptions = {
+      publishAudio: true,
+      publishVideo: false,
+      height: 128,
+      width: 128,
+      name: user
+    };
+  var publisher = TB.initPublisher(event.apikey, publisherContainer, pubOptions);
+  sessions[currentRoom].publish(publisher);
+  console.log('Pub Room: ' + room);
+  console.log('Pub currentRoom: ' + currentRoom);
+  // Subscribe to streams that were in the session when we connected
+  subscribeToStreams(event.streams);
+}
 
-        var publisherContainer = document.getElementById('publisherContainer');
-        // This example assumes that a publisherContainer div exists
-        publisherContainer.appendChild(div);
-
-        //set publishing options
-	var pubOptions = { 
-			publishAudio:true
-			, publishVideo: false
- 			, height: 128
-            		, width: 128
-            		, name: user
-	};
-        var publisher = TB.initPublisher(event.apikey, publisherContainer, pubOptions); 
-        sessions[currentRoom].publish(publisher);
-        console.log("Pub Room: " + room);
-        console.log("Pub currentRoom: " + currentRoom);
-
-        // Subscribe to streams that were in the session when we connected
-/*	if (subscribed == false) {
-		subscribed = true;
-        	subscribeToStreams(event.streams);
-	}
-*/
-                subscribeToStreams(event.streams);
-
-   }
-   function streamAvailableHandler(event) {
-   	console.log("streamAvailableHandler: no camera or microphone"); 
-   }
+function streamAvailableHandler(event) {
+  console.log('streamAvailableHandler: no camera or microphone');
+}
--- app/public/js/opentok2vivox.js
+++ app/public/js/opentok2vivox.js
@@ -1,169 +1,154 @@
-
 //Vivox functions that could be called by or have been called from unity web player
-var prevRoom = "";
+var prevRoom = '';
 var session;
-var room = "unassigned";
+var room = 'unassigned';
 var user;
 var gData;
-
 //initial vars
 var vvxHandle = {};
-
 vvxHandle = {
-    "_accounts": {
-        "15b642c2-19ed-44aa-80be-e171202a03f3": {
-            "AccountHandle": "15b642c2-19ed-44aa-80be-e171202a03f3",
-            "Uri": "sip:.90b11c633f65_2013102314160476785300.@regp.vivox.com",
-            "DisplayName": "Rick%20Support",
-            "IsAnonymousLogin": true,
-            "State": "1",
-            "WavDestinationPath": "C:\\Users\\rianders\\Documents\\"
-        }
+  '_accounts': {
+    '15b642c2-19ed-44aa-80be-e171202a03f3': {
+      'AccountHandle': '15b642c2-19ed-44aa-80be-e171202a03f3',
+      'Uri': 'sip:.90b11c633f65_2013102314160476785300.@regp.vivox.com',
+      'DisplayName': 'Rick%20Support',
+      'IsAnonymousLogin': true,
+      'State': '1',
+      'WavDestinationPath': 'C:\\Users\\rianders\\Documents\\'
     }
-}
-
-
-
-    
-    //var voiceChannelAddress = "sip:confctl-158@regp.vivox.com";
+  }
+};
+//var voiceChannelAddress = "sip:confctl-158@regp.vivox.com";
 var isLoggingIn = true;
-
-
 var opentokdivs = '<div id="devicePanelContainer"></div>\n<div id="publisherContainer"></div>\n<div id="streamsContainer"></div>';
 
-
 //functino from vivox
-
 function VivoxUnityInit() {
-	//If possible this is where the OpenTok init code should go
-    console.log("VivoxUnityInit");
-    console.log("VivoxUnityInit: Start");
-    var callbackFunctions = {
-        onConnected: vivoxConnected
-	 , onParticipantAdded: ParticipantAdded
-	 , onParticipantRemoved: ParticipantRemoved
-	 , onParticipantUpdated: ParticipantUpdated
-	 , onVersionCheck: VersionCheck
+  //If possible this is where the OpenTok init code should go
+  console.log('VivoxUnityInit');
+  console.log('VivoxUnityInit: Start');
+  var callbackFunctions = {
+      onConnected: vivoxConnected,
+      onParticipantAdded: ParticipantAdded,
+      onParticipantRemoved: ParticipantRemoved,
+      onParticipantUpdated: ParticipantUpdated,
+      onVersionCheck: VersionCheck
     };
-    console.log("VivoxUnityInit: Callback functions success");
-    console.log("VivoxJoinedRoom:start");
-    GetUnity().SendMessage("VivoxHud", "VivoxJoinedRoom", "");
-    console.log("VivoxJoinedRoom:end");
-    //GetUnity().SendMessage("RaiseHand", "VivoxJoinedRoom", "");
-
-    //triggers vivoxLogin event and assigns user name
-    GetUnity().SendMessage("VivoxHud", "onVivoxConnected", "Connected to Vivox network!");
-
-    //vvxHandle not enabled
-    /*vvxHandle
-    vvxHandle = new Vivox.API('https://www.regp.vivox.com/api2/',
-				{
-				    callbacks: callbackFunctions,
-				    //autoDownload: false,
-				    pluginVersion: '0.0.0',
-				    channelUri: voiceChannelAddress,
-				    //ephemeralId: voiceChannelAddress
-				});
-				console.log("VivoxUnityInit: Finished!" + vvxHandle);
-    // alert("Init!");
-    */
-         
-    	  console.log("VivoxUnityInit: end");
-    }
+  console.log('VivoxUnityInit: Callback functions success');
+  console.log('VivoxJoinedRoom:start');
+  GetUnity().SendMessage('VivoxHud', 'VivoxJoinedRoom', '');
+  console.log('VivoxJoinedRoom:end');
+  //GetUnity().SendMessage("RaiseHand", "VivoxJoinedRoom", "");
+  //triggers vivoxLogin event and assigns user name
+  GetUnity().SendMessage('VivoxHud', 'onVivoxConnected', 'Connected to Vivox network!');
+  console.log('VivoxUnityInit: end');
+}
 
 function VersionCheck(event) {
-    console.log("VersionCheck event" + event);
+  console.log('VersionCheck event' + event);
 }
 
-function vivoxConnected(Event) { console.log("vivoxConnected: " + Event ); }
-
-function HandleMuting(isMuted) { console.log("HandleMuting: " + isMuted); }
-
-function VivoxLogin(player) { 
-	user = player;
-	if (isLoggingIn) {
-	isLoggingIn = false;
- 	$.ajax({
-          dataType: "json",
-          url: "/api/" + world + "/" + user + "/" + room,
-          success: function(data) {
-              globaldata = $.extend({},  data);
-              sessions = data.sessions;
-
-              for (key in sessions) {
-                  console.log("Key: " + key + " data: " + data.sessions[key].sessionId);
-                  var session = TB.initSession(data.sessions[key].sessionId);
-                  sessions[key] = session;
-                  sessions[key].addEventListener("sessionConnected", sessionConnectedHandler);
-                  sessions[key].addEventListener("streamCreated", streamCreatedHandler);
-              }
-	      sessions[room].connect(globaldata.apikey,globaldata.tokens[room]);
-	      $("#rooms").text("Room: "+ room);
-          }
-      });
-	}
-	  console.log("OpenTOK url: " + "/api/" + "001/" + user + "/" + currentRoom );
-	console.log("VivoxLoginEnd: " + player); 
+function vivoxConnected(Event) {
+  console.log('vivoxConnected: ' + Event);
 }
 
-function SwitchToChannel(newChannel) { 
-	//This is where the vivox session disconnect and connect code should go.
-	prevRoom = currentRoom;
-	console.log("prevRoom: " + prevRoom);
-	console.log("SwitchToChannel: " + newChannel);
-
-	//Determine if there is a new room
-        GetUnity().SendMessage("VivoxHud", "UpdateCurrentChannel", newChannel);
-	if (newChannel == "sip:confctl-157@regp.vivox.com"){
-		console.log("Management: room3");
-		currentRoom = "room3";
- 	}
-	if (newChannel == "sip:confctl-31@regp.vivox.com"){
-		console.log("Union: room1");
-		currentRoom = "room1";
- 	}
-	if (newChannel == "sip:confctl-158@regp.vivox.com"){
-		console.log("Negotiation: room2");
-		currentRoom = "room2";
- 	}
-	//If room acutally changes switch
-  	if (prevRoom != currentRoom) {
-		sessions[prevRoom].disconnect();
-		$("#opentok").append("<div id='publisherContainer' />");
-		sessions[currentRoom].connect(globaldata.apikey,globaldata.tokens[currentRoom]);
-	      $("#rooms").text("Room: " + currentRoom);
-	}
-	console.log("SwitchToChannelComplete");
+function HandleMuting(isMuted) {
+  console.log('HandleMuting: ' + isMuted);
 }
 
+function VivoxLogin(player) {
+  user = player;
+  if (isLoggingIn) {
+    isLoggingIn = false;
+    $.ajax({
+      dataType: 'json',
+      url: '/api/' + world + '/' + user + '/' + room,
+      success: function (data) {
+        globaldata = $.extend({}, data);
+        sessions = data.sessions;
+        for (key in sessions) {
+          console.log('Key: ' + key + ' data: ' + data.sessions[key].sessionId);
+          var session = TB.initSession(data.sessions[key].sessionId);
+          sessions[key] = session;
+          sessions[key].addEventListener('sessionConnected', sessionConnectedHandler);
+          sessions[key].addEventListener('streamCreated', streamCreatedHandler);
+        }
+        sessions[room].connect(globaldata.apikey, globaldata.tokens[room]);
+        $('#rooms').text('Room: ' + room);
+      }
+    });
+  }
+  console.log('OpenTOK url: ' + '/api/' + '001/' + user + '/' + currentRoom);
+  console.log('VivoxLoginEnd: ' + player);
+}
 
-function installLocation() { console.log("installLocation"); }
+function SwitchToChannel(newChannel) {
+  //This is where the vivox session disconnect and connect code should go.
+  prevRoom = currentRoom;
+  console.log('prevRoom: ' + prevRoom);
+  console.log('SwitchToChannel: ' + newChannel);
+  //Determine if there is a new room
+  GetUnity().SendMessage('VivoxHud', 'UpdateCurrentChannel', newChannel);
+  if (newChannel == 'sip:confctl-592@regp.vivox.com') {
+    console.log('Management: room3');
+    currentRoom = 'room3';
+  }
+  if (newChannel == 'sip:confctl-593@regp.vivox.com') {
+    console.log('Union: room1');
+    currentRoom = 'room1';
+  }
+  if (newChannel == 'sip:confctl-591@regp.vivox.com') {
+    console.log('Negotiation: room2');
+    currentRoom = 'room2';
+  }
+  //If room acutally changes switch
+  if (prevRoom != currentRoom) {
+    sessions[prevRoom].disconnect();
+    $('#opentok').append('<div id=\'publisherContainer\' />');
+    sessions[currentRoom].connect(globaldata.apikey, globaldata.tokens[currentRoom]);
+    $('#rooms').text('Room: ' + currentRoom);
+  }
+  console.log('SwitchToChannelComplete');
+}
 
-function VivoxInstall() { console.log("VivoxInstall"); }
-
-function vivoxCompletedLogin(Response) { console.log("vivoxCompletedLogin: " + Response); }
-
-function ParticipantAdded(event) { console.log("ParticipantAdded: " + event); }
-
-function ParticipantRemoved(event) { console.log("ParticipantRemoved: " + event); }
-
-function ParticipantUpdated(event) { console.log("ParticipantUpdated: " + event); }
-
-function VivoxCreateChannel(channelName) { console.log("VivoxCreateChannel: " + channelName); }
-
-function vivoxChannelCreate(Response) { console.log("vivoxChannelCreate: " + Response); }
-
-function VivoxJoinChannel(channelURI, fontId) { console.log("VivoxJoinChannel: " + channelURI + " fintId: " + fontId); }
-
-function VivoxLogout(channelURI) { 
-	console.log("VivoxLogout: " + channelURI);
-	//added commented out vivox onvivoxlogout
-	GetUnity().SendMessage("VivoxHud", "onVivoxLogout");
-
- }
-
-function vivoxCompletedLogout(Response) { console.log("vivoxCompletedLogout: " + Response); }
-
-function VivoxMicMute(mute) { console.log("VivoxMicMute: " + mute ); }
-
-function vivoxMicMuteResult(response) { console.log("vivoxMicMuteResult: " + response); }
+function installLocation() {
+  console.log('installLocation');
+}
+function VivoxInstall() {
+  console.log('VivoxInstall');
+}
+function vivoxCompletedLogin(Response) {
+  console.log('vivoxCompletedLogin: ' + Response);
+}
+function ParticipantAdded(event) {
+  console.log('ParticipantAdded: ' + event);
+}
+function ParticipantRemoved(event) {
+  console.log('ParticipantRemoved: ' + event);
+}
+function ParticipantUpdated(event) {
+  console.log('ParticipantUpdated: ' + event);
+}
+function VivoxCreateChannel(channelName) {
+  console.log('VivoxCreateChannel: ' + channelName);
+}
+function vivoxChannelCreate(Response) {
+  console.log('vivoxChannelCreate: ' + Response);
+}
+function VivoxJoinChannel(channelURI, fontId) {
+  console.log('VivoxJoinChannel: ' + channelURI + ' fintId: ' + fontId);
+}
+function VivoxLogout(channelURI) {
+  console.log('VivoxLogout: ' + channelURI);
+  //added commented out vivox onvivoxlogout
+  GetUnity().SendMessage('VivoxHud', 'onVivoxLogout');
+}
+function vivoxCompletedLogout(Response) {
+  console.log('vivoxCompletedLogout: ' + Response);
+}
+function VivoxMicMute(mute) {
+  console.log('VivoxMicMute: ' + mute);
+}
+function vivoxMicMuteResult(response) {
+  console.log('vivoxMicMuteResult: ' + response);
+}
