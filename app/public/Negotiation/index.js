6cd4d040704ecf134cefec50aedb130cb167b6b1
Ryan Ostrager <ryan.ostrager@gmail.com>
2013-12-18 13:55:08 +0000

    Changed default room as a workaround to fix Admin not joining correct room. The other rooms fired the room join event properly, but Admin does not for some reason.

--- app/public/Negotiation/index.js
+++ app/public/Negotiation/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   console.log('Ready page Start');
   world = '001';
-  room = 'room1';
+  room = 'room2';
   currentRoom = room;  //Maybe declare all sessions when ready with event handlers.
                        //Then join various sessions on click
 });
